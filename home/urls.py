1ea6bb144133db6047a747910196a208640c865a
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-08-23 19:01:23 +0100

    Add about.html + url, and css files for home app

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="home/index.html"),name='home'),
+    path('about/', TemplateView.as_view(template_name="home/about.html"),name='about'),
 ]
