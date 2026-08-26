5f8367e33e6a8ffbdca3d70b6db3eac309295383
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-08-26 21:55:02 +0100

    Add summernote and admin class for Gym model

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+
 from .models import Gym,ContactSubmission
 
 # Register your models here.
 
-admin.site.register(Gym)
+@admin.register(Gym)
+class GymAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description','address','opening_hours')
+
 admin.site.register(ContactSubmission)
