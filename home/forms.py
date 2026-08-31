fa218ccf8a4656937a892459dbd864649603e9ad
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-08-31 22:25:39 +0100

    Add new styles for forms, allauth forms and media queries. Update ContactUsForm to incude placeholders

--- home/forms.py
+++ home/forms.py
@@ -12,15 +12,29 @@ class ContactForm(forms.ModelForm):
             'message',
         ]
 
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email',
+            'phone': 'Phone number',
+            'subject': 'Subject',
+            'message': 'Message',
+        }
+
         for field in self.fields.keys():
             self.fields[field].required = True
             if field == 'message':
                 self.fields[field].widget.attrs.update({
-                    'rows': 4, 'cols': 50
+                    'rows': 4, 'cols': 50,
+                    'class': 'form-control',
+                    'placeholder': placeholders[field],
+
                 })
 
             self.fields[field].widget.attrs.update({
                 'class': 'form-control',
+                'placeholder': placeholders[field],
             })
