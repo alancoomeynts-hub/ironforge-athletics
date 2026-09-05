099e98a66b20046e8a261857f25e89b557fabd84
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-05 19:33:02 +0100

    Add edit profile template and view

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('edit_profile/', views.edit_profile, name='edit_profile')
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,6 @@
-from django.db.models.fields import return_None
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import UserEditForm, ProfileEditForm
 from .models import User, Profile
 
 # Create your views here.
@@ -20,3 +20,19 @@ def dashboard(request):
             'profile': profile
         })
 
+@login_required
+def edit_profile(request):
+    user=request.user
+    profile=user.profile
+    if request.method == 'POST':
+        user_form=UserEditForm(request.POST, instance=request.user)
+        profile_form=ProfileEditForm(request.POST,instance=request.user)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form=UserEditForm(instance=request.user)
+        profile_form=ProfileEditForm(instance=profile)
+
+    return render(request,"user_profile/edit_profile.html",{'user_form':user_form,'profile_form':profile_form})
