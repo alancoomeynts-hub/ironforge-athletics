7b4e9fc36e026e20a67a165d3b045f0ea1efae90
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-05 19:32:41 +0100

    Add  forms for User and Profile

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Profile
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 @admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
+class ProfileAdmin(SummernoteModelAdmin):
     list_display = ('user','member_since','date_of_birth')
     list_filter = ('user','member_since')
     search_fields = ('user__username','user__first_name','user__last_name','default_phone_number','user__email')
     raw_id_fields = ('user',)
+    summernote_fields = ('bio','goals')
--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django import forms
 from .models import Profile
 from allauth.account.forms import SignupForm, LoginForm
@@ -10,7 +11,7 @@ class CustomSignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
         super(CustomSignupForm, self).__init__(*args, **kwargs)
 
-        for field in self.fields.keys():
+        for field in self.fields:
             self.fields[field].widget.attrs.update({
                 'class': 'form-control',
             })
@@ -30,8 +31,57 @@ class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super(CustomLoginForm, self).__init__(*args, **kwargs)
 
-        for field in self.fields.keys():
+        for field in self.fields:
             if field != 'remember':
                 self.fields[field].widget.attrs.update({
                     'class': 'form-control',
                 })
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ["username",
+                  "first_name",
+                  "last_name",
+                  "email"
+                  ]
+        widgets = {
+            "username": forms.TextInput(attrs={"placeholder": "Enter your username","class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"placeholder": "Enter your first name","class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Enter your last name","class": "form-control"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Enter your email","class": "form-control"}),
+        }
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [
+            "profile_image",
+            "bio",
+            "goals",
+            "date_of_birth",
+            "height",
+            "weight",
+            "default_phone_number",
+            "default_street_address1",
+            "default_street_address2",
+            "default_town_or_city",
+            "default_county",
+            "default_postcode",
+        ]
+        widgets = {
+            "profile_image": forms.FileInput(attrs={"class": "form-control"}),
+            "bio": forms.Textarea(attrs={"placeholder": "Enter your bio","class": "form-control",'rows': 4, 'cols': 50,}),
+            "goals": forms.Textarea(attrs={"placeholder": "Enter your goals","class": "form-control",'rows': 4, 'cols': 50,}),
+            "date_of_birth": forms.DateInput(attrs={"type": "date","class": "form-control"}),
+            "height": forms.NumberInput(attrs={"placeholder": "Enter your height","class": "form-control",}),
+            "weight": forms.NumberInput(attrs={"placeholder": "Enter your weight","class": "form-control",}),
+            "default_phone_number": forms.TextInput(attrs={"placeholder": "Enter your phone number","class": "form-control"}),
+            "default_street_address1": forms.TextInput(attrs={"placeholder": "Enter your street address","class": "form-control"}),
+            "default_street_address2": forms.TextInput(attrs={"placeholder": "Enter your street address","class": "form-control"}),
+            "default_town_or_city": forms.TextInput(attrs={"placeholder": "Enter your town or city","class": "form-control"}),
+            "default_county": forms.TextInput(attrs={"placeholder": "Enter your county","class": "form-control"}),
+            "default_postcode": forms.TextInput(attrs={"placeholder": "Enter your postcode","class": "form-control"}),
+
+        }
+
