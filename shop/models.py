721674fca8b17b992f8f856e5c37a5cb506450c6
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-06 19:52:49 +0100

    Add Shop app and product, product review and category models

---
+++ shop/models.py
@@ -0,0 +1,49 @@
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255,unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    sku = models.CharField(max_length=255,null=True,blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products')
+    image = CloudinaryField('image')
+    is_available = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews',null=True,blank=True)
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_on',)
+
+    def __str__(self):
+        return f'{self.product.name} review ({self.rating}/5)'
