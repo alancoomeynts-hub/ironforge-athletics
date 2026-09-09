648878a803d4c77f46cabafc24189a9b6677f373
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-09 22:56:56 +0100

    Add product_detail template, styles, url and view.

--- shop/views.py
+++ shop/views.py
@@ -9,11 +9,14 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+    products=products.order_by('name')
     return render(
         request,
         'shop/product_list.html',
         {'category': category, 'categories': categories, 'products': products}
     )
 
+def product_detail(request, id, slug):
+    product=get_object_or_404(Product, id=id, slug=slug, is_available=True)
 
-#
+    return render(request,"shop/product_detail.html",{'product':product})
