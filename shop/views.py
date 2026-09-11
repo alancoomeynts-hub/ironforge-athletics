fb96a9551b99cfc10a58c23582e4bd36896c7abe
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-11 09:18:03 +0100

    Add cart views, form and urls

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from django.views.decorators.http import require_POST
 from .models import Category,Product, ProductReview
+from .cart import Cart
+from .forms import CartAddProductForm
 
 # Create your views here.
 def product_list(request, category_slug=None):
@@ -18,5 +21,26 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product=get_object_or_404(Product, id=id, slug=slug, is_available=True)
+    cart_product_form=CartAddProductForm()
+    return render(request,"shop/product_detail.html",{'product':product,'cart_product_form':cart_product_form})
 
-    return render(request,"shop/product_detail.html",{'product':product})
+def cart_detail(request):
+    cart=Cart(request)
+    return render(request,'shop/cart.html',{'cart':cart})
+
+@require_POST
+def cart_add(request,product_id):
+    cart=Cart(request)
+    product=get_object_or_404(Product,id=product_id)
+    form=CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd=form.cleaned_data
+        cart.add_to_cart(product=product,quantity=cd['quantity'],override_quantity=cd['override'])
+    return redirect('shop:cart_detail')
+
+@require_POST
+def cart_remove(request,product_id):
+    cart=Cart(request)
+    product=get_object_or_404(Product,id=product_id)
+    cart.remove_from_cart(product)
+    return redirect('shop:cart_detail')
