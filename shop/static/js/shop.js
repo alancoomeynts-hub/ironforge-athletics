39414e26eea131ddad18ed2d119dd7871e99c081
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-12 12:46:37 +0100

    Add cart template, update shop.css and shop.js.

---
+++ shop/static/js/shop.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Initialize Bootstrap tooltips
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl)
+        });
+
+    });
