198c194742f1313760d2f1b97366e770c92b1afe
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-09-12 13:27:24 +0100

    refactor shop templates, cart form and styles, urls to use shop namespace and apply DJLint formatting

--- shop/forms.py
+++ shop/forms.py
@@ -3,5 +3,5 @@ from django import forms
 QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int, widget=forms.Select(attrs={'class': 'form-select mb-3'}))
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
